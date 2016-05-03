4a84eb652cd70b0bd0411b9e205ab3a9a64858a5
Shane Cowen <dopefish89@gmail.com>
2016-05-02 21:01:25 -0700

    Making a person!

---
+++ makeAPerson.js
@@ -0,0 +1,30 @@
+
+var Person = function(firstAndLast) {
+    var fullName = firstAndLast;
+    arr = fullName.split(' ');
+
+    this.getFirstName = function() {
+      return arr[0];
+    };
+    this.getLastName = function() {
+      return arr[1];
+    };
+    this.getFullName = function() {
+      return fullName;
+    };
+    this.setFirstName = function(first) {
+      arr[0] = first;
+      fullName = arr.join(' ');
+    };
+    this.setLastName = function(last) {
+      arr[1] = last;
+      fullName = arr.join(" ");
+    };
+    this.setFullName = function(firstAndLast) {
+      fullName = firstAndLast;
+      arr = fullName.split(' ');
+    };
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
